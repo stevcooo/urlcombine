9b25366c23ed42158a373045ee9a0fb9db54978c
Stevan <stevcooo@gmail.com>
2023-08-18 14:54:54 +0200

    Save

--- ChromeExtension/script.js
+++ ChromeExtension/script.js
@@ -1,23 +1,3 @@
-var _AnalyticsCode = 'UA-27205339-5';
-
-/**
- * Below is a modified version of the Google Analytics asynchronous tracking
- * code snippet.  It has been modified to pull the HTTPS version of ga.js
- * instead of the default HTTP version.  It is recommended that you use this
- * snippet instead of the standard tracking snippet provided when setting up
- * a Google Analytics account.
- */
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', _AnalyticsCode]);
-_gaq.push(['_trackPageview']);
-(function() {
-  var ga = document.createElement('script');
-  ga.type = 'text/javascript';
-  ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(ga, s);
-})();
 /**
  * Track a click on a button using the asynchronous tracking API.
  *
@@ -25,72 +5,66 @@ _gaq.push(['_trackPageview']);
  * for information on how to use the asynchronous tracking API.
  */
 function trackButtonClick(e) {
-  _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+  _gaq.push(["_trackEvent", e.target.id, "clicked"]);
 }
 
-document.addEventListener("DOMContentLoaded",function(event) {
-  document.getElementById("openUrlBtn").addEventListener("click",openUrl);
-  document.getElementById("settingsToggle").addEventListener("click",toggleSettingsControls);
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.getElementById("openUrlBtn").addEventListener("click", openUrl);
+  document.getElementById("settingsToggle").addEventListener("click", toggleSettingsControls);
 
-  document.getElementById("mainUrl").addEventListener("focusout",storeSettings);
-  document.getElementById("prefixId").addEventListener("focusout",storeSettings);
-  
-  document.getElementsByTagName("BODY")[0].style.width="400px";
-  document.getElementsByTagName("html")[0].style.height="120px";
+  document.getElementById("mainUrl").addEventListener("focusout", storeSettings);
+  document.getElementById("prefixId").addEventListener("focusout", storeSettings);
+
+  document.getElementsByTagName("BODY")[0].style.width = "400px";
+  document.getElementsByTagName("html")[0].style.height = "120px";
   document.getElementsByClassName("settings").hidden = true;
-  chrome.storage.local.get('prefixId', function(result) {
-    if(result.prefixId != undefined)
-      document.getElementById('prefixId').value = result.prefixId;
+  chrome.storage.local.get("prefixId", function (result) {
+    if (result.prefixId != undefined) document.getElementById("prefixId").value = result.prefixId;
   });
-  
-  chrome.storage.local.get('mainUrl', function(result) {
-    if(result.mainUrl != undefined)
-      document.getElementById('mainUrl').value = result.mainUrl;
-  });  
+
+  chrome.storage.local.get("mainUrl", function (result) {
+    if (result.mainUrl != undefined) document.getElementById("mainUrl").value = result.mainUrl;
+  });
 });
 
-document.addEventListener("keyup",function(event) {
-    if (event.keyCode === 13) {
-      document.getElementById("openUrlBtn").click();
-    }
-  });
+document.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    document.getElementById("openUrlBtn").click();
+  }
+});
 
 function openUrl() {
-    var mainUrl = document.getElementById('mainUrl').value;
-    var prefixId = document.getElementById('prefixId').value;
-    var taskId = document.getElementById('taskId').value;
+  var mainUrl = document.getElementById("mainUrl").value;
+  var prefixId = document.getElementById("prefixId").value;
+  var taskId = document.getElementById("taskId").value;
 
-    if(mainUrl!=undefined){
-      storeSettings();
+  if (mainUrl != undefined) {
+    storeSettings();
 
-      var destinationUrl = mainUrl + prefixId + taskId;
-      if(!destinationUrl.startsWith("http")){
-        destinationUrl = "http://" + destinationUrl;
-      }
-
-      chrome.tabs.create({url: destinationUrl});
-  
-      //Google analytics
-      _gaq.push(['_trackEvent', 'OpenUrlBtn', 'clicked']);
+    var destinationUrl = mainUrl + prefixId + taskId;
+    if (!destinationUrl.startsWith("http")) {
+      destinationUrl = "http://" + destinationUrl;
     }
-}
 
-function storeSettings(){
-    var mainUrl = document.getElementById('mainUrl').value;
-    var prefixId = document.getElementById('prefixId').value;
-
-    chrome.storage.local.set({'prefixId': prefixId});
-    chrome.storage.local.set({'mainUrl': mainUrl});
-}
-
-function toggleSettingsControls(){
-  var isHidden = document.getElementsByClassName("settings").hidden;
-  if(isHidden){
-    document.getElementsByTagName("html")[0].style.height="290px";  
-    document.getElementsByClassName("settings").hidden = false;
+    chrome.tabs.create({ url: destinationUrl });
   }
-  else{
-    document.getElementsByTagName("html")[0].style.height="120px";
-    document.getElementsByClassName("settings").hidden = true;    
+}
+
+function storeSettings() {
+  var mainUrl = document.getElementById("mainUrl").value;
+  var prefixId = document.getElementById("prefixId").value;
+
+  chrome.storage.local.set({ prefixId: prefixId });
+  chrome.storage.local.set({ mainUrl: mainUrl });
+}
+
+function toggleSettingsControls() {
+  var isHidden = document.getElementsByClassName("settings").hidden;
+  if (isHidden) {
+    document.getElementsByTagName("html")[0].style.height = "290px";
+    document.getElementsByClassName("settings").hidden = false;
+  } else {
+    document.getElementsByTagName("html")[0].style.height = "120px";
+    document.getElementsByClassName("settings").hidden = true;
   }
 }
