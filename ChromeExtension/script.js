ee5749250baa80ac97732b5acd64740b76f4fb1d
Stevan Kostoski <stevcooo@gmail.com>
2025-03-03 15:45:34 +0100

    save

--- ChromeExtension/script.js
+++ ChromeExtension/script.js
@@ -11,20 +11,44 @@ function trackButtonClick(e) {
 document.addEventListener("DOMContentLoaded", function (event) {
   document.getElementById("openUrlBtn").addEventListener("click", openUrl);
   document.getElementById("settingsToggle").addEventListener("click", toggleSettingsControls);
+  document.getElementById("addProjectPrefixBtn").addEventListener("click", addProjectPrefix);
 
   document.getElementById("mainUrl").addEventListener("focusout", storeSettings);
-  document.getElementById("prefixId").addEventListener("focusout", storeSettings);
 
   document.getElementsByTagName("BODY")[0].style.width = "400px";
-  document.getElementsByTagName("html")[0].style.height = "108px";
+  //document.getElementsByTagName("html")[0].style.height = "108px";
   document.getElementsByClassName("settings").hidden = true;
-  chrome.storage.local.get("prefixId", function (result) {
-    if (result.prefixId != undefined) document.getElementById("prefixId").value = result.prefixId;
-  });
 
   chrome.storage.local.get("mainUrl", function (result) {
     if (result.mainUrl != undefined) document.getElementById("mainUrl").value = result.mainUrl;
   });
+
+  chrome.storage.local.get("prefixes", function (result) {
+    console.log("prefixes", result.prefixes);
+    if (!result.prefixes) return;
+   
+    var prefixes = result.prefixes;
+    var prefixesContainer = document.getElementById("prefixesContainer");
+    for (var i = 0; i < prefixes.length; i++) {
+      var prefix = prefixes[i];
+      var div = document.createElement("div");
+      div.className = "field";
+      var input = document.createElement("input");
+      input.type = "text";
+      input.value = prefix;
+      input.className = "projectPrefixInput";
+      input.addEventListener("focusout", storeSettings);
+      div.appendChild(input);
+      var label = document.createElement("label");
+      label.htmlFor = "prefixId";
+      var span = document.createElement("span");
+      span.innerHTML = "Project prefix";
+      label.appendChild(span);
+      div.appendChild(label);
+      prefixesContainer.appendChild(div);
+    }
+  });
+
 });
 
 document.addEventListener("keyup", function (event) {
@@ -33,29 +57,71 @@ document.addEventListener("keyup", function (event) {
   }
 });
 
-function openUrl() {
-  var mainUrl = document.getElementById("mainUrl").value;
-  var prefixId = document.getElementById("prefixId").value;
+async function openUrl() {
+  let mainUrl = await getStorageSync("mainUrl");
+  if (!mainUrl) return;
+  
+  let prefixes = await getStorageSync("prefixes");
+  if(!prefixes || prefixes.length === 0) return;
+  
   var taskId = document.getElementById("taskId").value;
 
-  if (mainUrl != undefined) {
-    storeSettings();
 
-    var destinationUrl = mainUrl + prefixId + taskId;
+  for (var i = 0; i < prefixes.length; i++) {
+    var destinationUrl = mainUrl + prefixes[i] + taskId;
     if (!destinationUrl.startsWith("http")) {
       destinationUrl = "http://" + destinationUrl;
     }
 
-    chrome.tabs.create({ url: destinationUrl });
+    if (!await isLinkBroken(destinationUrl)) {
+          chrome.tabs.create({ url: destinationUrl });
+    } else {
+      console.log("Link is broken", destinationUrl);
+    }
   }
+}
+
+async function addProjectPrefix() {
+  var prefixesContainer = document.getElementById("prefixesContainer");
+  var div = document.createElement("div");
+  div.className = "field";
+  var input = document.createElement("input");
+  input.type = "text";
+  input.className = "projectPrefixInput";
+  input.addEventListener("focusout", storeSettings);
+  div.appendChild(input);
+  var label = document.createElement("label");
+  label.htmlFor = "prefixId";
+  var span = document.createElement("span");
+  span.innerHTML = "Project prefix";
+  label.appendChild(span);
+  div.appendChild(label);
+  prefixesContainer.appendChild(div);
+}
+
+async function isLinkBroken(url) {
+    try {
+      const response = await fetch(`https://broken-link-checker-1xea.vercel.app/check?url=${url}`);
+      console.log("response", response);
+      return !response.ok; // Returns true if the link is broken
+    } catch (error) {
+        return true; // If fetch fails (network error, CORS issue, etc.), consider the link broken
+    }
 }
 
 function storeSettings() {
   var mainUrl = document.getElementById("mainUrl").value;
-  var prefixId = document.getElementById("prefixId").value;
-
-  chrome.storage.local.set({ prefixId: prefixId });
   chrome.storage.local.set({ mainUrl: mainUrl });
+
+  // Here I need to collect all values from the inputs with class `projectPrefixInput`
+  // and store them in the storage
+  var projectPrefixInputs = document.getElementsByClassName("projectPrefixInput");
+  var prefixes = [];
+  for (var i = 0; i < projectPrefixInputs.length; i++) {
+    const value = projectPrefixInputs[i].value;
+    if (value) prefixes.push(value);
+  }
+  chrome.storage.local.set({ prefixes: prefixes });
 }
 
 function toggleSettingsControls() {
@@ -67,4 +133,12 @@ function toggleSettingsControls() {
     document.getElementsByTagName("html")[0].style.height = "108px";
     document.getElementsByClassName("settings").hidden = true;
   }
+}
+
+function getStorageSync(key) {
+    return new Promise((resolve) => {
+        chrome.storage.local.get([key], function (result) {
+            resolve(result[key]);
+        });
+    });
 }
